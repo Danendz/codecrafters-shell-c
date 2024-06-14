ca96a80935a132e7bfa68c4f5c98329c8e9e16a6
@disa.danila <disa.danila@ad-srv.multibank.uz>
2024-06-14 14:42:33 +0500

    Missing commands handling

--- app/main.c
+++ app/main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-   printf("$ ");
-   fflush(stdout);
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+    // Wait for user input
+    char input[101];
+    fgets(input, 100, stdin);
+
+    size_t ln = strlen(input) - 1;
+
+    if (input[ln] == '\n') {
+        input[ln] = '\0';
+    }
+
+    printf("%s: command not found", input);
+    return 0;
 }
