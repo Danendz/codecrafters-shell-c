d891a56d35cb40f79fb6d5e0bcef4257864bd4c7
@disa.danila <disa.danila@ad-srv.multibank.uz>
2024-06-14 17:15:50 +0500

    Echo command

--- app/main.c
+++ app/main.c
@@ -18,23 +18,42 @@ int main() {
         }
 
         char *command = (char*)malloc(sizeof(char) * ln);
+        char *params = NULL;
+
         int len = 0;
+        int space_i = -1;
 
         for (int i = 0; i < ln; i++) {
             if (input[i] == ' ') {
+                space_i = i+1;
                 break;
             }
 
             command[i] = input[i];
             len++;
         }
+
         command[len] = '\0';
 
-        if (strcmp(command, "exit") == 0) {
-            break;
+        if (space_i != -1) {
+            size_t params_len = ln - space_i;
+            params = (char*)malloc(sizeof(char) * params_len);
+
+            for (int i = space_i, j = 0; i < ln; i++, j++) {
+                params[j] = input[i];
+            }
+
+            params[params_len] = '\0';
         }
 
-        printf("%s: command not found", input);
+        if (strcmp(command, "echo") == 0 && params != NULL) {
+            printf("%s", params);
+        } else if(strcmp(command, "exit") == 0) {
+            break;
+        } else {
+            printf("%s: command not found", input);
+        }
+
         printf("\n$ ");
         fflush(stdout);
     }
