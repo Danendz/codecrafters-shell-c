bc35484482137f35a36f650f265c0ec2c0486b3d
@disa.danila <disa.danila@ad-srv.multibank.uz>
2024-06-20 10:54:27 +0500

    Refactor

--- app/main.c
+++ app/main.c
@@ -3,7 +3,7 @@
 #include <malloc.h>
 #include <stdbool.h>
 
-void readWriteInput(char* input, int count, bool newLine) {
+void readWriteInput(char *input, int count, bool newLine) {
     if (newLine) {
         printf("\n");
     }
@@ -12,8 +12,49 @@ void readWriteInput(char* input, int count, bool newLine) {
     fgets(input, count, stdin);
 }
 
+void getCommandAndParams(const char *input, char *command, char **params, size_t ln) {
+    int len = 0;
+    int space_i = -1;
+
+    for (int i = 0; i < ln; i++) {
+        if (input[i] == ' ') {
+            space_i = i + 1;
+            break;
+        }
+
+        command[i] = input[i];
+        len++;
+    }
+
+    command[len] = '\0';
+
+    if (space_i != -1) {
+        size_t params_len = ln - space_i;
+        *params = (char *) malloc(sizeof(char) * params_len);
+
+        for (int i = space_i, j = 0; i < ln; i++, j++) {
+            (*params)[j] = input[i];
+        }
+
+        (*params)[params_len] = '\0';
+    }
+}
+
+int readCommand(const char *command, char *params) {
+    if (strcmp(command, "exit") == 0) {
+        return -1;
+    } else if(strcmp(command, "echo") == 0 && params != NULL) {
+        printf("%s", params);
+    } else if(strcmp(command, "type") == 0 && params != NULL) {
+        printf("%s", params);
+    } else {
+        printf("%s: command not found", command);
+    }
+
+    return 0;
+}
+
 int main() {
-    // Wait for user input
     char input[101];
     readWriteInput(input, 100, false);
 
@@ -24,43 +65,15 @@ int main() {
             input[ln] = '\0';
         }
 
-        char *command = (char*)malloc(sizeof(char) * ln);
+        char *command = (char *) malloc(sizeof(char) * ln);
         char *params = NULL;
 
-        int len = 0;
-        int space_i = -1;
+        getCommandAndParams(input, command, &params, ln);
 
-        for (int i = 0; i < ln; i++) {
-            if (input[i] == ' ') {
-                space_i = i+1;
-                break;
-            }
+        int res = readCommand(command, params);
 
-            command[i] = input[i];
-            len++;
-        }
-
-        command[len] = '\0';
-
-        if (space_i != -1) {
-            size_t params_len = ln - space_i;
-            params = (char*)malloc(sizeof(char) * params_len);
-
-            for (int i = space_i, j = 0; i < ln; i++, j++) {
-                params[j] = input[i];
-            }
-
-            params[params_len] = '\0';
-        }
-
-        if (strcmp(command, "echo") == 0 && params != NULL) {
-            printf("%s", params);
-        } else if(strcmp(command, "type") == 0 && params != NULL)  {
-            printf("%s", params);
-        } else if(strcmp(command, "exit") == 0) {
+        if (res == -1) {
             break;
-        } else {
-            printf("%s: command not found", input);
         }
 
         readWriteInput(input, 100, true);
